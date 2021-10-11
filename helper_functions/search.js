33ce7a3a6e70e24e28ac9162c8c11cd2ce180108
Peter Smid <petosmid@gmail.com>
2021-10-11 14:08:17 +0200

    main search implemented

---
+++ helper_functions/search.js
@@ -0,0 +1,54 @@
+import obceSK from '../data/obceSK.js';
+import geomorphological from '../data/geomorphological'
+const NR_OF_RESULTS = 5
+
+
+export function searchAllTypes(query) {
+  const normalizedQuery = normalizeDiacritics(query.toLowerCase());
+  const villages = searchVillages(normalizedQuery)
+  const geomorphounits = searchGeomorphological(normalizedQuery);
+
+  if (geomorphounits.length > 0){
+    return [...geomorphounits, ...villages]
+  }
+  return villages
+}
+
+function searchVillages(query) {
+  const results = [];
+  let count = 0;
+  for (let village of obceSK) {
+    const normalizedName = normalizeDiacritics((village.name).toLowerCase())
+    if (count < NR_OF_RESULTS) {
+      if (normalizedName.startsWith(query)) {
+        count += 1;
+        results.push({...village, type: 'village'});
+      }
+    } else {
+    }
+  }
+  return results;
+}
+
+function searchGeomorphological(query) {
+  const results = [];
+  let count = 0;
+  for (let unit of geomorphological) {
+    const normalizedName = normalizeDiacritics((unit.name).toLowerCase())
+    if (count < NR_OF_RESULTS) {
+      if (normalizedName.startsWith(query)) {
+        count += 1;
+        results.push(unit);
+      }
+    } else {
+    }
+  }
+  return results;
+}
+
+
+function normalizeDiacritics(query) {
+  return (
+    query.normalize("NFD").replace(/[\u0300-\u036f]/g, '')
+  )
+}
