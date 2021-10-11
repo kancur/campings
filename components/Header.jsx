cb02b8d65362352e79089d11d0aff0e687313c2b
Peter Smid <petosmid@gmail.com>
2021-10-11 12:41:17 +0200

    Implemented grid as a building block for header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
+import Button from "./building_blocks/Button";
 
 export default function Header(props) {
   return (
-    <div className="bg-green-500 p-6 flex items-center justify-center flex-col gap-6">
-      <div className="flex items-center gap-2">
-        <h1 className="text-4xl	text-gray-50 font-bold">Najkempy.sk</h1>
+    <div className="bg-green-500 p-4">
+      <div className="grid grid-cols-3 items-center gap-2">
+        <div></div>
+        <div className="flex justify-center">
+          <h1 className="text-4xl	text-gray-50 font-bold">Najkempy.sk</h1>
+        </div>
+        <div className="flex justify-end">
+        </div>
       </div>
       {props.children}
     </div>
