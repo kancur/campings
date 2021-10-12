7870ba1dba4c2aa205a943208b8e8ef744c27938
Peter Smid <petosmid@gmail.com>
2021-10-12 10:34:59 +0200

    refractoring and css improvements

---
+++ components/most_popular_camps_showcase/PopularCampsWrapper.jsx
@@ -0,0 +1,24 @@
+import PopularCampCard from './PopularCampCard';
+
+const FAKE_DATA = [
+  {
+    name: 'trusalova ATC',
+    location: 'Velka Fatra',
+    img: 'trusalova.jpg',
+  },
+  {
+    name: 'random kemp',
+    location: 'Domasa',
+    img: 'opatovce.jpg',
+  },
+];
+
+export default function PopularCampsWrapper() {
+  return (
+    <div className="grid grid-cols-6 gap-4 px-5">
+      {FAKE_DATA.map((campData) => (
+        <PopularCampCard campData={campData} />
+      ))}
+    </div>
+  );
+}
