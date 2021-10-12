9954afacf58423bb5ea887329a37223fc2bc3d44
Peter Smid <petosmid@gmail.com>
2021-10-12 10:35:25 +0200

    refractored to not take children anymore

--- components/search/SearchWrapper.jsx
+++ components/search/SearchWrapper.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Image from 'next/image';
 //import background from '../../public/media/trusalova.jpg';
 import background from '../../public/media/trusalova.jpg';
+import SearchBar from './SearchBar';
 
-export default function SearchWrapper(props) {
+export default function SearchWrapper() {
   return (
     <>
       <div /* style={{backgroundImage: `url(/media/bonfire.jpg)`}} */ className="p-12 bg-gray-400">
-        {/* <Image src={background} /> */}
-        <div className="text-center flex justify-center">{props.children}</div>
+        <div className="text-center flex justify-center">
+          <SearchBar />
+        </div>
       </div>
     </>
   );
