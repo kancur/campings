99f92307da7d465876d077427f4acc98d62706e7
Peter Smid <petosmid@gmail.com>
2021-10-17 19:44:17 +0200

    location heading design changes

--- components/locationPage/LocationHeading.jsx
+++ components/locationPage/LocationHeading.jsx
@@ -1,10 +1,9 @@
-export default function LocationHeading({ pretitle, title }) {
+export default function LocationHeading({ pretitle, title, subtitle }) {
   return (
-    <div className="text-center">
-      <p>{pretitle}</p>
-      <h1 className="text-5xl font-bold text-gray-600 p-3">
-        {title}
-      </h1>
+    <div className="text-center p-5 bg-gray-100">
+      {pretitle && <p className="text-xl text-gray-500">{pretitle}</p>}
+      <h1 className="text-6xl font-bold text-gray-600 p-2">{title}</h1>
+      {subtitle && <p className="text-gray-500">Okres {subtitle}</p>}
     </div>
   );
 }
