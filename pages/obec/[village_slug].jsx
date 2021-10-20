5a4f38441100c9af910b34092e7c48aa8371d951
Peter Smid <petosmid@gmail.com>
2021-10-20 23:08:10 +0200

    initial camp display prototype

--- pages/obec/[village_slug].jsx
+++ pages/obec/[village_slug].jsx
@@ -1,23 +1,40 @@
-import { useRouter } from 'next/router';
+import Image from 'next/image';
 import Main from '../../components/Main';
 import Search from '../../components/search/Search';
 import LocationHeading from '../../components/locationPage/LocationHeading';
 
-const VillagePage = ({ data }) => {
-  const router = useRouter();
-  const { locationName } = router.query;
-
+const VillagePage = ({ village, campings }) => {
   return (
     <>
       <Search />
       <Main>
         <LocationHeading
-          pretitle="Skvelé kempy v obci"
-          title={data.name}
-          subtitle={data.parents.county_name}
+          pretitle="Skvelé kempy neďaleko obce"
+          title={village.name}
+          subtitle={village.parents.county_name}
         />
-        Hey, you visited a location with slug {locationName}
-        <div>Slug: {data.name}</div>
+
+        <div className="p-4 flex flex-col gap-4 max-w-screen-md mx-auto">
+          {campings.map((camp) => (
+            <div className="shadow-lg border border-opacity-40 p-5 bg-white flex gap-4">
+              <Image src="/camp_placeholder.png" height={200} width={200} />
+              <div>
+                <h2 className="text-2xl font-semibold">{camp.name}</h2>
+                <div className="flex">
+                  <p className="text-gray-400">
+                    {camp.villages[0].name}, {'okres '}
+                    {camp.villages[0].parents.county_name}
+                  </p>
+                </div>
+                <p className="text-gray-500">
+                  {Math.round(camp.distance / 1000)} km
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div></div>
       </Main>
     </>
   );
@@ -37,17 +54,33 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
+  const {
+    VILLAGE_CAMP_MAX_DISTANCE,
+    VILLAGE_CAMP_MAX_RESULTS,
+  } = require('../../OPTIONS');
   const village_slug = params.village_slug;
   const encodedName = encodeURI(village_slug);
 
-  const res = await fetch(
-    `http://localhost:3000/api/village/slug/${encodedName}`
+  // cant do following requests in parallel because the second one needs data from the first one
+
+  const villageRes = await fetch(
+    `${process.env.DB_HOST}/api/village/slug/${encodedName}`
   );
-  const data = await res.json();
+  const villageData = await villageRes.json();
+
+  const closeCampingsURL = new URL(`${process.env.DB_HOST}/api/camping/close`);
+  closeCampingsURL.searchParams.append('lat', villageData.coords[1]);
+  closeCampingsURL.searchParams.append('lon', villageData.coords[0]);
+  closeCampingsURL.searchParams.append('limit', VILLAGE_CAMP_MAX_RESULTS);
+  closeCampingsURL.searchParams.append('distance', VILLAGE_CAMP_MAX_DISTANCE);
+  const campingsRes = await fetch(closeCampingsURL.toString());
+  const campingsData = await campingsRes.json();
+
   return {
     props: {
-      data,
-    }, // will be passed to the page component as props
+      village: villageData,
+      campings: campingsData,
+    },
   };
 }
 
