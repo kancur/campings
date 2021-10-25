28428b8233434bcfae8454b089198c05259a2eba
Peter Smid <petosmid@gmail.com>
2021-10-25 17:37:38 +0200

    change colors and lint

--- components/admin/Sidebar.jsx
+++ components/admin/Sidebar.jsx
@@ -5,12 +5,11 @@ import { useRouter } from 'next/dist/client/router';
 export default function AdminSidebar() {
   return (
     <ul className="flex flex-col gap-y-3">
-      <MenuItem href="/admin/add-new" text="Add new campsite">
-        <FaPlusCircle className="text-green-300" />
+      <MenuItem href="/admin/camps" text="All campsites">
+        <FaListUl className="text-white" />
       </MenuItem>
-
-      <MenuItem href="/admin/camp-list" text="All campsites">
-        <FaListUl className="text-green-300" />
+      <MenuItem href="/admin/camps/new" text="Add new campsite">
+        <FaPlusCircle className="text-white" />
       </MenuItem>
     </ul>
   );
@@ -18,10 +17,14 @@ export default function AdminSidebar() {
 
 function MenuItem({ children, href, text }) {
   const router = useRouter();
-  const isActive = (router.pathname == href)
+  const isActive = router.pathname == href;
 
   return (
-    <li className={` p-3 rounded-lg flex items-center gap-x-2 text-gray-50 text-xl ${isActive ? "bg-gray-500" : ""}`}>
+    <li
+      className={` p-3 rounded-lg flex items-center gap-x-2 text-gray-50 text-xl ${
+        isActive ? 'bg-gray-500' : ''
+      }`}
+    >
       {children}
       <Link href={href}>{text}</Link>
     </li>
