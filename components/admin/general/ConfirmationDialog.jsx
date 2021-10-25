e4e0481f3d84ecc7ea9a0463c045e2ed126826ce
Peter Smid <petosmid@gmail.com>
2021-10-25 17:45:12 +0200

    fix button import

--- components/admin/general/ConfirmationDialog.jsx
+++ components/admin/general/ConfirmationDialog.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Portal } from 'react-portal';
-import { ButtonAdmin } from './ButtonAdmin';
+import ButtonAdmin from './ButtonAdmin';
 import OutsideClickHandler from 'react-outside-click-handler';
 import FocusLock from 'react-focus-lock';
 
