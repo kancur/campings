85170ed8fa083e2304e5b5bf1abbd4ba14eb0953
Peter Smid <petosmid@gmail.com>
2021-10-31 10:30:01 +0100

    add support for preview image

--- components/admin/camps/new/CampPreview.jsx
+++ components/admin/camps/new/CampPreview.jsx
@@ -1,11 +1,11 @@
 import { CampListing } from '../../../general/CampListing';
 
-export default function CampPreview({camp}) {
+export default function CampPreview({camp, previewImage}) {
   return (
     <div className="flex flex-col gap-1">
       <h2 className="text-xl">Preview</h2>
       <div className="mx- flex p-4 border-2 border-dashed  border-gray-300 rounded-lg justify-center">
-        <CampListing camp={camp} />
+        <CampListing camp={camp} previewImage={previewImage} />
       </div>
     </div>
   );
