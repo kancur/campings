8b49899954d35f100394222c0fb794f126887f5d
Peter Smid <petosmid@gmail.com>
2021-10-31 11:42:54 +0100

    handle preview images

--- components/general/CampListing.jsx
+++ components/general/CampListing.jsx
@@ -1,6 +1,11 @@
 import Image from 'next/image';
+import { DB_HOST } from '../../OPTIONS';
 
-export function CampListing({ camp }) {
+export function CampListing({ camp, previewImage }) {
+  console.log('preview image:', camp.featured_image)
+
+  const featured = `${DB_HOST}/${camp.featured_image}`
+
   const parentVillage = camp.villages ? camp.villages[0] : null;
   const distance = camp.distance
     ? `${Math.round(camp.distance / 1000)} km`
@@ -8,12 +13,14 @@ export function CampListing({ camp }) {
 
   return (
     <div className="shadow-md border border-gray-100 p-3 bg-white flex gap-4 rounded-md max-w-screen-md flex-1">
+      
       <Image
         className="object-cover"
-        src={camp.featuredImage ? camp.featuredImage : '/camp_placeholder.png'}
+        src={previewImage ? previewImage : camp.featured_image ? featured : '/camp_placeholder.png'}
         height={200}
         width={200}
       />
+
       <div>
         <h2 className="text-2xl font-semibold">{camp?.name}</h2>
         <div className="flex">
