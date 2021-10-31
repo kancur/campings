82959c25432aef296eea9b7b5d72cb2b47f62c2c
Peter Smid <petosmid@gmail.com>
2021-10-31 10:31:35 +0100

    fix edge cases

--- helpers/toSlug.js
+++ helpers/toSlug.js
@@ -1,9 +1,12 @@
 export default function toSlug(name) {
   if (name) {
-    const lowercased = name.toLowerCase()
-    const normalized = lowercased.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-    const hyphenized = normalized.split(' ').join('-').toString();
-    return hyphenized
+    const lowercased = name.toLowerCase();
+    const trimmed = lowercased.trim();
+    const normalized = trimmed
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
+    const hyphenized = normalized.replace(/[^\w ]+/g, '').replace(/ +/g, '-');
+    return hyphenized;
   }
-  return ""
+  return '';
 }
