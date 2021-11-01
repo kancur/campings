a149d0e76993794e66ba70ed87644e0d10bbe246
Peter Smid <petosmid@gmail.com>
2021-11-01 09:18:02 +0100

    delete hardcoded subtitle value

--- components/locationPage/LocationHeading.jsx
+++ components/locationPage/LocationHeading.jsx
@@ -3,7 +3,7 @@ export default function LocationHeading({ pretitle, title, subtitle }) {
     <div className="text-center p-4 bg-gray-100 text-gray-500">
       {pretitle && <p className="text-lg">{pretitle}</p>}
       <h1 className="text-5xl font-bold text-gray-600 p-2">{title}</h1>
-      {subtitle && <p>Okres {subtitle}</p>}
+      {subtitle && <p>{subtitle}</p>}
     </div>
   );
 }
