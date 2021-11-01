87d0e8b307bb9fcd4b75eacb114acbdbdf3ea8ac
Peter Smid <petosmid@gmail.com>
2021-11-01 09:19:29 +0100

    refractor and design changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,23 @@
 import Head from 'next/head';
 import PopularCampsWrapper from '../components/most_popular_camps_showcase/PopularCampsWrapper';
-import SearchForm from '../components/search/SearchForm';
 import Search from '../components/search/Search';
 
 export default function Home(props) {
+
   return (
     <div className="flex flex-col justify-center">
       <Head>
         <title>Najkempy.sk</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Search searchDatabase={props.data}/>
-      <section className="pb-10">
-        <h1 className="text-3xl text-center p-5">Populárne kempy</h1>
-        <PopularCampsWrapper />
+      <Search searchDatabase={props.data} />
+      <section className="p-4">
+        
+          <div>
+            <h1 className="text-3xl text-center p-5">Populárne kempy</h1>
+            <PopularCampsWrapper />
+          </div>
+        
       </section>
     </div>
   );
