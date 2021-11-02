dfb911776253d5e5e61718f9df8b4ec2ad60235f
Peter Smid <petosmid@gmail.com>
2021-11-02 15:32:12 +0100

    implement search page

--- pages/search/index.jsx
+++ pages/search/index.jsx
@@ -1,17 +1,38 @@
 import Search from '../../components/search/Search';
 import { useRouter } from 'next/router';
 import LocationHeading from '../../components/locationPage/LocationHeading';
+import { useEffect, useState } from 'react';
+import { campSearch } from '../../helpers/search';
+import { CampListing } from '../../components/general/CampListing';
 
 export default function SearchPage() {
+  const [results, setResults] = useState();
   const router = useRouter();
-  const {q} = router.query
+  const { q } = router.query;
+
+  useEffect(() => {
+    if (q && q.length >= 3) {
+      campSearch(q)
+        .then((res) => {
+          if (res?.error) {
+            console.error(res.error);
+          }
+          if (Array.isArray(res)) {
+            setResults(res);
+          }
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [q]);
+
   return (
     <>
       <Search />
-      <LocationHeading
-          pretitle="Výsledky vyhľadávania pre výraz"
-          title={q}
-        />
+      <LocationHeading pretitle="Výsledky vyhľadávania pre výraz" title={q} />
+      <div className="camp-listing-wrapper">
+        {results &&
+          results.map((camp) => <CampListing key={camp._id} camp={camp} />)}
+      </div>
     </>
   );
 }
