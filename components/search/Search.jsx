8730c7c36e5882d1a31494a5d14b8e8af03b3555
Peter Smid <petosmid@gmail.com>
2021-11-02 08:21:13 +0100

    refractor

--- components/search/Search.jsx
+++ components/search/Search.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import background from '../../public/media/trusalova.jpg';
 import SearchForm from './SearchForm';
 import PopularLocations from './PopularLocations';
 
 export default function Search() {
+
   return (
     <>
       <div /* style={{backgroundImage: `url(/media/bonfire.jpg)`}} */ className="p-6 bg-green-500">
         <div className="text-center flex flex-col items-center justify-center space-y-5">
-          <SearchForm />
-          {/* <PopularLocations /> */}
+          <SearchForm />          {/* <PopularLocations /> */}
         </div>
       </div>
     </>
