c93f45193062507b2ed92e9b5ec2b968283c89b8
Peter Smid <petosmid@gmail.com>
2021-11-03 18:34:10 +0100

    add popular destinations

---
+++ data/popularDestinations.js
@@ -0,0 +1,17 @@
+export const mountains = [
+  {name: 'Vysoké Tatry', slug: 'tatry'},
+  {name: 'Malá Fatra', slug: 'mala-fatra'},
+  {name: 'Pieniny', slug: 'pieniny'},
+]
+
+export const towns = [
+  {name: 'Liptovský Mikuláš', slug: 'liptovsky-mikulas'},
+  {name: 'Terchová', slug: 'terchova'},
+  {name: 'Banská Štiavnica', slug: 'banska-stiavnica'},
+]
+
+export const waterBodies = [
+  {name: 'Oravská priehrada', slug: 'oravska-priehrada'},
+  {name: 'Liptovská Mara', slug: 'liptovska-mara'},
+  {name: 'Počúvadlo', slug: 'pocuvadlo'},
+]
