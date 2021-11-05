aa11bf7664c49107964951c1126488be80041c71
Peter Smid <petosmid@gmail.com>
2021-11-05 22:18:16 +0100

    add handling of different states visually

--- pages/search/index.jsx
+++ pages/search/index.jsx
@@ -4,20 +4,29 @@ import LocationHeading from '../../components/locationPage/LocationHeading';
 import { useEffect, useState } from 'react';
 import { campSearch } from '../../helpers/search';
 import { CampListing } from '../../components/general/CampListing';
+import { ImSpinner, ImSpinner2 } from 'react-icons/im';
+import LoaderJumpingTents from '../../components/general/LoaderJumpingTents';
 
 export default function SearchPage() {
-  const [results, setResults] = useState();
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
+
   const router = useRouter();
   const { q } = router.query;
 
   useEffect(() => {
+    setIsLoading(true);
+    setResults([]);
     if (q && q.length >= 3) {
       campSearch(q)
         .then((res) => {
           if (res?.error) {
+            setIsLoading(false);
             console.error(res.error);
           }
           if (Array.isArray(res)) {
+            setIsLoading(false);
             if (res.length > 0) {
               setResults(res);
             }
@@ -31,10 +40,17 @@ export default function SearchPage() {
     <>
       <SearchWrapper />
       <LocationHeading pretitle="Výsledky vyhľadávania pre výraz" title={q} />
-      <div className="camp-listing-wrapper">
-        {results &&
+      <div className="camp-listing-wrapper h-full w-full">
+        {isLoading && <LoaderJumpingTents className="my-auto" />}
+
+        {results.length > 0 &&
           results.map((camp) => <CampListing key={camp._id} camp={camp} />)}
-        {!results && <div>Pre tvoje hľadanie sme nenašli žiadny kemp.😟</div>}
+
+        {results.length === 0 && !isLoading && (
+          <p className="mx-auto m-4 text-base sm:text-lg">Pre tvoju frázu sme nenašli žiadny kemp.😟</p>
+        )}
+
+        {error && <p>Pri vyhľadávaní došlo k chybe. 😟</p>}
       </div>
     </>
   );
