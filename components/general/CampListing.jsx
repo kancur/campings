376625da69c35837e44a2d1375df95ef5131a682
Peter Smid <petosmid@gmail.com>
2021-11-05 22:20:33 +0100

    fix - switch from flex to grid and refractor

--- components/general/CampListing.jsx
+++ components/general/CampListing.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import { BACKEND_HOST } from '../../OPTIONS';
 
 export function CampListing({ camp, previewImage }) {
-  const featured = `${BACKEND_HOST}/${camp.featured_image}`
+  const featured = `${BACKEND_HOST}/${camp.featured_image}`;
 
   const parentVillage = camp.villages ? camp.villages[0] : null;
   const distance = camp.distance
@@ -10,16 +10,23 @@ export function CampListing({ camp, previewImage }) {
     : '';
 
   return (
-    <div className="shadow-md border border-gray-100 p-3 bg-white flex gap-4 rounded-md max-w-screen-md flex-1">
-      
-      <Image
-        className="object-cover"
-        src={previewImage ? previewImage : camp.featured_image ? featured : '/camp_placeholder.png'}
-        height={200}
-        width={200}
-      />
-
-      <div>
+    <div className="shadow-md border border-gray-100 p-3 bg-white grid gap-4 grid-cols-3 rounded-md max-w-screen-md flex-1">
+      <div className="col-span-1 relative">
+        <div className="w-48 h-48">
+          <Image
+            layout="fill"
+            objectFit="cover"
+            src={
+              previewImage
+                ? previewImage
+                : camp.featured_image
+                ? featured
+                : '/camp_placeholder.png'
+            }
+          />
+        </div>
+      </div>
+      <div className="col-span-2">
         <h2 className="text-2xl font-semibold">{camp?.name}</h2>
         <div className="flex">
           <p className="text-gray-400 text-s mb-2">
