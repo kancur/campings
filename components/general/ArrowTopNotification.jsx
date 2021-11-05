4989d39c984d403c5216893164c619971e9c39f4
Peter Smid <petosmid@gmail.com>
2021-11-05 22:20:50 +0100

    move to another folder

--- components/general/ArrowTopNotification.jsx
+++ components/general/ArrowTopNotification.jsx
@@ -2,7 +2,7 @@ export function ArrowTopNotification({ children }) {
   return (
     <div className="absolute left-1/2 -translate-x-1/2 mt-2 bg-red-600 p-2 text-white rounded shadow-md z-10 select-none">
       {children}
-      <div className="arrow-up absolute left-1/2 -top-2 -translate-x-1/2 border-red-600 z-30"></div>
+      <div className="arrow-up absolute left-1/2 -top-2 -translate-x-1/2 text-red-600 z-20"></div>
     </div>
   );
 }
