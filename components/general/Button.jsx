d4a7d0d42acf0aaf4a5f93a373b15795b892536f
Peter Smid <petosmid@gmail.com>
2021-11-08 23:46:37 +0100

    tidy up code

--- components/general/Button.jsx
+++ components/general/Button.jsx
@@ -1,7 +1,6 @@
-const Button = ({ children }) => (
+export const Button = ({ children }) => (
   <button className="bg-pink-700 p-2 rounded-xl text-gray-100 font-semibold hover:bg-pink-500 focus:ring-2">
     {children}
   </button>
 );
 
-export default Button;
