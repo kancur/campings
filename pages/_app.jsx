1bd60abbb11da5a0fa1452b20f2e24828a0eedc8
Peter Smid <petosmid@gmail.com>
2021-11-08 23:44:55 +0100

    add authentication pipeline

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,17 +1,29 @@
 import '../styles/tailwind.css';
 import Layout from '../components/base/Layout';
+import { CookiesProvider } from 'react-cookie';
+import { AuthProvider } from '../context/authContext';
 
-function MyApp({ Component, pageProps }) {
+function WrappedWithCoorectLayout({ Component, pageProps }) {
   const wrappedComponent = Component.Layout ? (
     <Component.Layout>
-        <Component {...pageProps} />
+      <Component {...pageProps} />
     </Component.Layout>
   ) : (
     <Layout>
-        <Component {...pageProps} />
+      <Component {...pageProps} />
     </Layout>
   );
   return wrappedComponent;
+}
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <CookiesProvider>
+      <AuthProvider>
+        {WrappedWithCoorectLayout({ Component, pageProps })}
+      </AuthProvider>
+    </CookiesProvider>
+  );
 }
 
 export default MyApp;
