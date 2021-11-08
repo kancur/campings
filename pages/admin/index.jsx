f713bd09db527d3f9ff246afa231f33e8e61d615
Peter Smid <petosmid@gmail.com>
2021-11-08 23:48:50 +0100

    refractor

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,13 +1,11 @@
-import dynamic from "next/dynamic"
-import AdminLayout from '../../components/admin/AdminLayout'
+import dynamic from 'next/dynamic';
+import AdminLayout from '../../components/admin/AdminLayout';
 
-const Admin = dynamic(() => import("../../components/admin/AdminPage"), {
+const Admin = dynamic(() => import('../../components/admin/AdminPage'), {
   ssr: false,
-})
+});
 
-const AdminPageComponent = () => <Admin />
+const AdminPageComponent = () => <Admin />;
+AdminPageComponent.Layout = AdminLayout;
 
-export default AdminPageComponent
-
-AdminPageComponent.Layout = AdminLayout
-  
+export default AdminPageComponent;
