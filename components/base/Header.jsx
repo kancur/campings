0eacaee35d52f06ef22cb4f5c14e55625ffa1abb
Peter Smid <petosmid@gmail.com>
2021-11-08 23:47:11 +0100

    overhaul the menu

--- components/base/Header.jsx
+++ components/base/Header.jsx
@@ -2,20 +2,22 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import Button from '../general/Button';
+import HeaderMenu from './HeaderMenu';
 
 export default function Header(props) {
   return (
     <header className="bg-gray-100 p-4 z-50">
-      <div className="grid grid-cols-3 items-center gap-2">
-        <div></div>
-        <div className="flex justify-center">
+      <div className="flex items-center gap-2">
+        <div className="flex flex-1 w-1/3">
           <Link href="/">
             <a>
-              <h1 className="text-4xl	text-brand font-bold">Najkempy.sk</h1>
+              <h1 className="text-4xl	text-brand font-bold px-4 py-1">Najkempy.sk</h1>
             </a>
           </Link>
         </div>
-        <div className="flex justify-end"></div>
+        <div className="flex flex-1 justify-end w-2/3">
+          <HeaderMenu />
+        </div>
       </div>
       {props.children}
     </header>
