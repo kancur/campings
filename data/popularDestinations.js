b553a576956046cefcf5a3cd57b21b94723bf620
Peter Smid <petosmid@gmail.com>
2021-11-08 23:45:11 +0100

    fix slugs

--- data/popularDestinations.js
+++ data/popularDestinations.js
@@ -1,17 +1,17 @@
 export const mountains = [
-  {name: 'Vysoké Tatry', slug: 'tatry'},
-  {name: 'Malá Fatra', slug: 'mala-fatra'},
-  {name: 'Pieniny', slug: 'pieniny'},
+  {name: 'Vysoké Tatry', slug: '/tatry'},
+  {name: 'Malá Fatra', slug: '/mala-fatra'},
+  {name: 'Pieniny', slug: '/pieniny'},
 ]
 
 export const towns = [
-  {name: 'Liptovský Mikuláš', slug: 'obec/liptovsky-mikulas'},
-  {name: 'Terchová', slug: 'obec/terchova'},
-  {name: 'Banská Štiavnica', slug: 'obec/banska-stiavnica'},
+  {name: 'Liptovský Mikuláš', slug: '/obec/liptovsky-mikulas'},
+  {name: 'Terchová', slug: '/obec/terchova'},
+  {name: 'Banská Štiavnica', slug: '/obec/banska-stiavnica'},
 ]
 
 export const waterBodies = [
-  {name: 'Oravská priehrada', slug: 'voda/oravska-priehrada'},
-  {name: 'Liptovská Mara', slug: 'voda/liptovska-mara'},
-  {name: 'Počúvadlo', slug: 'voda/pocuvadlianske-jazero'},
+  {name: 'Oravská priehrada', slug: '/voda/oravska-priehrada'},
+  {name: 'Liptovská Mara', slug: '/voda/liptovska-mara'},
+  {name: 'Počúvadlo', slug: '/voda/pocuvadlianske-jazero'},
 ]
