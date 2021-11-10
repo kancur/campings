2c965fa999e984622af2ae999021d0ead7d6d099
Peter Smid <petosmid@gmail.com>
2021-11-10 18:22:22 +0100

    add logout functionality

--- components/base/Menu/MobileMenu.jsx
+++ components/base/Menu/MobileMenu.jsx
@@ -5,19 +5,28 @@ import {
   LoggedInMenuItems,
   LoggedOutMenuItems,
 } from './MenuItems';
+import { useCookies } from 'react-cookie';
+import { useRouter } from 'next/router';
 
-function MobileMenu() {
+function MobileMenu({ close }) {
   const auth = useAuth();
+
+  const handleMenuItemClick = () => {
+    close();
+  };
 
   return (
     <div>
-      <nav className="p-2 bg-gray-50">
-        <ul className="font-semibold text-gray-600 flex flex-col gap-1">
-          <CommonMenuItems />
-          {!auth.user && <LoggedOutMenuItems auth={auth} />}
-        </ul>
-      </nav>
-      {auth.user && <UserInformationMobile auth={auth} />}
+        <nav className="p-2 bg-gray-50">
+          <ul
+            className="font-semibold text-gray-600 flex flex-col gap-1"
+            onClick={handleMenuItemClick}
+          >
+            <CommonMenuItems />
+            {!auth.user && <LoggedOutMenuItems auth={auth} />}
+          </ul>
+        </nav>
+        {auth.user && <UserInformationMobile auth={auth} />}
     </div>
   );
 }
@@ -25,6 +34,14 @@ function MobileMenu() {
 export default MobileMenu;
 
 function UserInformationMobile({ auth }) {
+  const [cookies, setCookie, removeCookie] = useCookies(['jwt']);
+  const router = useRouter();
+
+  const handleRemoveCookie = () => {
+    removeCookie('jwt');
+    router.reload(window.location.pathname);
+  };
+
   return (
     <div className="px-4">
       <div className="border-t-2 border-gray-200 py-4 text-sm text-gray-600 space-y-2">
@@ -32,7 +49,12 @@ function UserInformationMobile({ auth }) {
           <span className="font-semibold">Tvoj email:</span>
           <span className="font-mono">{auth.user.email}</span>
         </div>
-        <button className="px-2 py-1 text-white font-semibold bg-red-500 hover:bg-red-400 rounded-md shadow-sm mx-auto block">Odhlásiť</button>
+        <button
+          onClick={handleRemoveCookie}
+          className="px-2 py-1 text-white font-semibold bg-red-500 hover:bg-red-400 rounded-md shadow-sm mx-auto block"
+        >
+          Odhlásiť
+        </button>
       </div>
     </div>
   );
