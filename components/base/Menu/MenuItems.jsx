04d79cd88cbccbc90d797b9f98a5a7e1f17b1cfb
Peter Smid <petosmid@gmail.com>
2021-11-16 11:19:27 +0100

    add logout button

--- components/base/Menu/MenuItems.jsx
+++ components/base/Menu/MenuItems.jsx
@@ -9,6 +9,7 @@ import {
 import { MenuLinkWrapper } from './MenuItemWrapper';
 import Link from 'next/link';
 import classNames from 'classnames';
+import { Dropdown } from '../../general/SearchSuggestionsDropdown';
 
 const iconclasses = classNames('w-5 h-5');
 
@@ -29,16 +30,24 @@ export function CommonMenuItems() {
 export function LoggedInMenuItems({ auth }) {
   return (
     <>
-      <MenuLinkWrapper className="flex items-center gap-1">
-        {auth.user?.email}
-        <FaChevronDown className={iconclasses} />
-      </MenuLinkWrapper>
-      {/*       <MenuItemWrapper>
-              <button type="button" className="flex items-center gap-1">
-                <FaSignOutAlt className={iconclasses} />
-                Odhlásiť
-              </button>
-            </MenuItemWrapper> */}
+      <div className="relative">
+        <MenuLinkWrapper className="flex items-center gap-1">
+          {auth.user?.email}
+          <FaChevronDown className="w-4 h-4" />
+        </MenuLinkWrapper>
+
+        <div
+          role="menu"
+          className="absolute mt-2 bg-gray-50 border border-gray-100 p-2 shadow-md rounded-md w-52 text-gray-600 z-20 left-2/4 -translate-x-2/4"
+        >
+          <button
+            onClick={auth.logout}
+            className="px-2 py-1 text-white font-semibold bg-red-500 hover:bg-red-400 rounded-md shadow-sm mx-auto block"
+          >
+            Odhlásiť
+          </button>
+        </div>
+      </div>
     </>
   );
 }
