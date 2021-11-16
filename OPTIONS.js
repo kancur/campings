9c20084d4f861b0468c7fcbbfb9361368738feed
Peter Smid <petosmid@gmail.com>
2021-11-16 18:41:19 +0100

    revert to direct api route for backend host

--- OPTIONS.js
+++ OPTIONS.js
@@ -2,4 +2,4 @@ exports.VILLAGE_CAMP_MAX_DISTANCE = 20000 // metres
 exports.VILLAGE_CAMP_MAX_RESULTS = 10 // items
 //exports.BACKEND_HOST= 'http://localhost:3000'
 //exports.BACKEND_HOST= 'http://192.168.0.200:3000'
-exports.BACKEND_HOST= 'https://najkempy.sk/api'
+exports.BACKEND_HOST= 'https://campings-express.herokuapp.com'
