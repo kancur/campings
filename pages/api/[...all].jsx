5905fac26df01b5620f73c9e6993c268b90b08ca
Peter Smid <petosmid@gmail.com>
2021-11-16 18:50:30 +0100

    fix route to match requests

--- pages/api/[...all].jsx
+++ pages/api/[...all].jsx
@@ -2,4 +2,8 @@ import httpProxyMiddleware from "next-http-proxy-middleware"
 
 export default (req, res) => httpProxyMiddleware(req, res, {
   target:'https://campings-express.herokuapp.com',
+  pathRewrite: [{
+    patternStr: '^/api/',
+    replaceStr: ''
+  }]
 });
