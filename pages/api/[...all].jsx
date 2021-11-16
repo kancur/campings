9a428dfeaed15f7d9e7067b4ff4f151fb8ba28eb
Peter Smid <petosmid@gmail.com>
2021-11-16 19:37:22 +0100

    fix middleware url

--- pages/api/[...all].jsx
+++ pages/api/[...all].jsx
@@ -1,7 +1,7 @@
 import httpProxyMiddleware from "next-http-proxy-middleware"
 
 export default (req, res) => httpProxyMiddleware(req, res, {
-  target:'https://campings-express.herokuapp.com/api',
+  target:'https://campings-express.herokuapp.com',
   /* pathRewrite: [{
     patternStr: '^/api/',
     replaceStr: '/'
