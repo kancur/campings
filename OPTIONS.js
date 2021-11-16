3569b0d53a682d95fd6fea1129be32a25bf7d701
Peter Smid <petosmid@gmail.com>
2021-11-16 19:24:26 +0100

    add environment detection

--- OPTIONS.js
+++ OPTIONS.js
@@ -2,5 +2,5 @@ exports.VILLAGE_CAMP_MAX_DISTANCE = 20000 // metres
 exports.VILLAGE_CAMP_MAX_RESULTS = 10 // items
 //exports.BACKEND_HOST= 'http://localhost:3000'
 //exports.BACKEND_HOST= 'http://192.168.0.200:3000'
-exports.BACKEND_HOST= 'https://campings-express.herokuapp.com'
-exports.FRONTEND_API_ROUTE='https://www.najkempy.sk/api'
+exports.BACKEND_HOST = 'https://campings-express.herokuapp.com'
+exports.FRONTEND_API_ROUTE = (process.env.NODE_ENV == 'development') ? 'http://localhost:3000' : 'https://www.najkempy.sk/api'
