7ce3a773f28f15a591d4502a2c49b808c00adae4
Peter Smid <petosmid@gmail.com>
2021-11-18 12:15:18 +0100

    add aws url to whitelisted domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   images: {
-    domains: ['campings-express.herokuapp.com','localhost'],
+    domains: [
+      'campings-express.herokuapp.com',
+      'localhost',
+      'campings-s3.s3.eu-central-1.amazonaws.com',
+    ],
   },
-  
-}
+};
