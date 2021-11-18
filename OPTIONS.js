3aff9313667b702ae2f919a1401d533f3bed151d
Peter Smid <petosmid@gmail.com>
2021-11-18 12:15:39 +0100

    add dev vs prod url switching

--- OPTIONS.js
+++ OPTIONS.js
@@ -1,6 +1,14 @@
-exports.VILLAGE_CAMP_MAX_DISTANCE = 20000 // metres
-exports.VILLAGE_CAMP_MAX_RESULTS = 10 // items
+exports.VILLAGE_CAMP_MAX_DISTANCE = 20000; // metres
+exports.VILLAGE_CAMP_MAX_RESULTS = 10; // items
 //exports.BACKEND_HOST= 'http://localhost:3000'
 //exports.BACKEND_HOST= 'http://192.168.0.200:3000'
-exports.BACKEND_HOST = 'https://campings-express.herokuapp.com'
-exports.FRONTEND_API_ROUTE = (process.env.NODE_ENV == 'development') ? 'http://localhost:3000' : 'https://www.najkempy.sk/api'
+exports.BACKEND_HOST =
+  process.env.NODE_ENV == 'development'
+    ? 'http://localhost:3000/api'
+    : 'https://campings-express.herokuapp.com';
+
+exports.STATIC_HOST ='https://campings-s3.s3.eu-central-1.amazonaws.com';
+exports.FRONTEND_API_ROUTE =
+  process.env.NODE_ENV == 'development'
+    ? 'http://localhost:3000/api'
+    : 'https://www.najkempy.sk/api';
