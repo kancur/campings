fe14d675b064cbdc4e746703c5b9561e8601a497
Peter Smid <petosmid@gmail.com>
2021-11-18 12:17:20 +0100

    add description

--- pages/api/[...all].jsx
+++ pages/api/[...all].jsx
@@ -1,6 +1,8 @@
 import httpProxyMiddleware from "next-http-proxy-middleware"
 
 export default (req, res) => httpProxyMiddleware(req, res, {
+  // the /api/ url is automatically captured annd forwarded with the api call route
+  // no need to add /api to the url manually
   target:'https://campings-express.herokuapp.com',
   /* pathRewrite: [{
     patternStr: '^/api/',
