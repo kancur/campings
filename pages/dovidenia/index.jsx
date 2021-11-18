c85960773f7dedeefcd1d224514bcb7da894fbb7
Peter Smid <petosmid@gmail.com>
2021-11-18 12:17:11 +0100

    change css classes

--- pages/dovidenia/index.jsx
+++ pages/dovidenia/index.jsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 export default function LoggedOutPage() {
   return (
-    <div className="flex flex-col items-center p-4 space-y-2">
-      <h1 className="text-3xl" >Bol si odhlásený</h1>
+    <div className="flex flex-col items-center p-4 space-y-3 my-auto">
+      <h1 className="text-4xl text-pink-500" >Bol si odhlásený</h1>
       <p>
         Prejdi na <Link href="/">domovskú stránku</Link>.
       </p>
