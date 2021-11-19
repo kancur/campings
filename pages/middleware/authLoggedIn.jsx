c64da0e4eeea5de7825df83a651ef94631bb5dac
Peter Smid <petosmid@gmail.com>
2021-11-19 23:29:56 +0100

    initial commit

---
+++ pages/middleware/authLoggedIn.jsx
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+import { BACKEND_HOST } from '../../OPTIONS';
+
+// checking if user is logged in
+export async function authLoggedIn(req) {
+  const cookies = req.cookies;
+  if (!cookies.jwt) return NextResponse.redirect('/prihlasenie');
+
+  try {
+    const response = await fetch(`${process.env.BACKEND_HOST}/api/auth/current-user`, {
+      headers: {
+        Cookie: `jwt=${cookies.jwt}`,
+      },
+    });
+
+    const user = await response.json();
+
+    if (user) {
+      return NextResponse.next();
+    }
+
+    return NextResponse.redirect('/prihlasenie');
+
+  } catch (error) {
+    return new Response(`Auth error: ${error.message}`, {
+      status: 401,
+    });
+  }
+}
