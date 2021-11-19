821a16a69df3e40479fa57812448d1a5339f2c50
Peter Smid <petosmid@gmail.com>
2021-11-19 12:07:36 +0100

    inital commit - context with previous path

---
+++ context/pathHistoryContext.js
@@ -0,0 +1,25 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+const previousPathContext = createContext();
+
+export function PreviousPathProvider({ children }) {
+  const router = useRouter();
+  const [pathHistory, setPathHistory] = useState([]);
+
+  useEffect(() => {
+    setPathHistory((prev) => [...prev, router.asPath]);
+  }, [router.asPath]);
+
+  const getPrevPath = () => pathHistory[pathHistory.length - 2];
+
+  return (
+    <previousPathContext.Provider value={pathHistory[pathHistory.length - 2]}>
+      {children}
+    </previousPathContext.Provider>
+  );
+}
+
+export function usePreviousPath() {
+  return useContext(previousPathContext);
+}
