1c48b7201edc2826afe6a87f2af0a5d3ec10fd8e
Peter Smid <petosmid@gmail.com>
2021-11-19 12:04:21 +0100

    change design

--- components/base/Footer.jsx
+++ components/base/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer(props) {
   return (
-    <div className="bg-gray-100 text-brand p-6 flex items-center justify-center flex-col gap-6 z-40">
-      Created by Peter
+    <div className="bg-gray-100 text-gray-500 text-sm p-4 flex items-center justify-center flex-col gap-6 z-40">
+      najkempy.sk © {new Date().getFullYear()} 
     </div>
   );
 }
