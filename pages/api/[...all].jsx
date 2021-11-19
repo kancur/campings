c12b92be76588f01794512c6966bdf1cd11117b6
Peter Smid <petosmid@gmail.com>
2021-11-19 12:08:19 +0100

    fix localhost dev target url

--- pages/api/[...all].jsx
+++ pages/api/[...all].jsx
@@ -3,9 +3,15 @@ import httpProxyMiddleware from "next-http-proxy-middleware"
 export default (req, res) => httpProxyMiddleware(req, res, {
   // the /api/ url is automatically captured annd forwarded with the api call route
   // no need to add /api to the url manually
-  target:'https://campings-express.herokuapp.com',
+  target: (process.env.NODE_ENV === 'development') ? 'http://localhost:3000' : 'https://campings-express.herokuapp.com'
   /* pathRewrite: [{
     patternStr: '^/api/',
     replaceStr: '/'
   }] */
 });
+
+export const config = {
+    api: {
+        bodyParser: false
+    }
+}
