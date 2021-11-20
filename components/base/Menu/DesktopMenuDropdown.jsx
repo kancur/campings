fca94d64165409061136be6cd6705eff8fcc8f4a
Peter Smid <petosmid@gmail.com>
2021-11-20 18:29:10 +0100

    add admin link for admins

--- components/base/Menu/DesktopMenuDropdown.jsx
+++ components/base/Menu/DesktopMenuDropdown.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTransition, animated } from 'react-spring';
-
+import Link from 'next/link';
+import classNames from 'classnames'
 export function DesktopMenuDropdown({ auth, isDropOpen }) {
   const transitions = useTransition(isDropOpen, {
     from: { opacity: 0 },
@@ -8,8 +9,11 @@ export function DesktopMenuDropdown({ auth, isDropOpen }) {
     leave: { opacity: 0 },
     reverse: isDropOpen,
     delay: 100,
-
   });
+
+  const listItemStyle = classNames(
+    'p-2'
+  )
 
   return transitions(
     (styles, item) =>
@@ -17,9 +21,15 @@ export function DesktopMenuDropdown({ auth, isDropOpen }) {
         <animated.div style={styles}>
           <div
             role="menu"
-            className="absolute mt-2 bg-white border border-gray-100 p-3 shadow-md rounded-md w-56 text-gray-600 z-20 -right-4 text-center space-y-2"
+            className="absolute mt-2 bg-white border border-gray-100 p-3 shadow-md rounded-md w-56 text-gray-600 z-20 -right-4 space-y-2 divide-y divide-gray-400"
           >
-            <p>{auth.user?.email}</p>
+            <div>
+              <p className="text-center text-sm font-semibold">Tvoj email:</p>
+              <p className="text-center text-sm">{auth.user?.email}</p>
+            </div>
+            <ul>
+            {auth.user?.is_admin && <li className={listItemStyle}><Link href="/admin">Administrácia</Link></li>}
+            </ul>
             <button
               onClick={auth.logout}
               className="px-2 py-1 text-white font-semibold bg-red-500 hover:bg-red-400 rounded-md shadow-sm mx-auto block"
