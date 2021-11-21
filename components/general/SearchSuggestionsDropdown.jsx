ed24f97e1724f3264491c702a95e2284c3b69b05
Peter Smid <petosmid@gmail.com>
2021-11-21 18:26:55 +0100

    fix z index issue

--- components/general/SearchSuggestionsDropdown.jsx
+++ components/general/SearchSuggestionsDropdown.jsx
@@ -2,7 +2,7 @@ export function Dropdown({ children }) {
 
   return (
     <div className="relative inline-block">
-      <div role="menu" className="absolute top-2 bg-gray-50 border border-gray-200 shadow-lg rounded-md w-52 text-gray-600 ring-black ring-1 ring-opacity-5 z-40">
+      <div role="menu" className="z-50 absolute top-2 bg-gray-50 border border-gray-200 shadow-lg rounded-md w-52 text-gray-600 ring-black ring-1 ring-opacity-5 z-40">
         {children}
       </div>
     </div>
