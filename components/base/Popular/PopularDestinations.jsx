7e005cc58bbffdcd493dab4c4066fae23bd4c41e
Peter Smid <petosmid@gmail.com>
2021-12-13 11:41:03 +0100

    refractor popular destinations

---
+++ components/base/Popular/PopularDestinations.jsx
@@ -0,0 +1,33 @@
+import { mountains, towns, waterBodies } from '../../../data/popularDestinations';
+import Link from 'next/link';
+
+export default function PopularDestinations() {
+  return (
+    <div className="grid sm:grid-cols-3 gap-2 text-center sm:text-left max-w-xl">
+      <DestinationWrapper title="Hory" destinations={mountains} />
+      <DestinationWrapper title="Obce" destinations={towns} />
+      <DestinationWrapper title="Vodné plochy" destinations={waterBodies} />
+    </div>
+  );
+}
+
+function DestinationWrapper({ title, destinations }) {
+  return (
+    <div className="space-y-1">
+      <h3 className="text-2xl font-semibold text-green-600">{title}</h3>
+      <DestinationList destinations={destinations} />
+    </div>
+  );
+}
+
+function DestinationList({ destinations }) {
+  return (
+    <ul>
+      {destinations.map((dest) => (
+        <li key={dest.slug}>
+          <Link href={dest.slug}>{dest.name}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import SearchWrapper from '../components/search/SearchWrapper';
 import { mountains, towns, waterBodies } from '../data/popularDestinations';
-import Link from 'next/link';
+import PopularDestinations from '../components/base/Popular/PopularDestinations';
 
 export default function Home(props) {
   return (
@@ -16,37 +16,10 @@ export default function Home(props) {
           <h2 className="text-3xl font-semibold text-center">
             Obľúbené destinácie
           </h2>
-          <div className="grid sm:grid-cols-3 gap-2 text-center sm:text-left">
-            <DestinationWrapper title="Hory" destinations={mountains} />
-            <DestinationWrapper title="Obce" destinations={towns} />
-            <DestinationWrapper
-              title="Vodné plochy"
-              destinations={waterBodies}
-            />
-          </div>
+          <PopularDestinations />
         </div>
       </section>
     </div>
   );
 }
 
-function DestinationWrapper({ title, destinations }) {
-  return (
-    <div className="space-y-1">
-      <h3 className="text-2xl font-semibold text-green-600">{title}</h3>
-      <DestinationList destinations={destinations} />
-    </div>
-  );
-}
-
-function DestinationList({ destinations }) {
-  return (
-    <ul>
-      {destinations.map((dest) => (
-        <li key={dest.slug}>
-          <Link href={dest.slug}>{dest.name}</Link>
-        </li>
-      ))}
-    </ul>
-  );
-}
