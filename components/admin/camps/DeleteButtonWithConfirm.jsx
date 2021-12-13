893637f9694d50ef8b9d5fb362b3b5161b062a89
Peter Smid <petosmid@gmail.com>
2021-12-13 22:17:57 +0100

    refractor and simplify

--- components/admin/camps/DeleteButtonWithConfirm.jsx
+++ components/admin/camps/DeleteButtonWithConfirm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
 import ButtonAdmin from '../general/ButtonAdmin';
 import ConfirmationDialog from '../general/ConfirmationDialog';
@@ -6,20 +6,28 @@ import ConfirmationDialog from '../general/ConfirmationDialog';
 const myRef = React.createRef();
 
 export default function DeleteButtonWithConfirm({ onConfirm, confirmationMsg }) {
+  const [isOpen, setIsOpen] = useState(false);
   const getConfirmation = async () => {
     const modal = myRef.current;
     return modal.show();
   };
 
   const handleClick = async () => {
-    getConfirmation()
-      .then(onConfirm)
-      .catch(() => {});
+    setIsOpen(true);
   };
+
+  const handleConfirm = () => {
+    onConfirm();
+    setIsOpen(false);
+  }
+
+  const handleCancel = () => {
+    setIsOpen(false);
+  }
 
   return (
     <>
-      <ConfirmationDialog ref={myRef} confirmationMsg={confirmationMsg} confirmationHeading="Confirm removal" actionBtnName="Delete" actionBtnClassName="bg-red-500"/>
+      <ConfirmationDialog show={isOpen} onConfirm={handleConfirm} onCancel={handleCancel} ref={myRef} confirmationMsg={confirmationMsg} confirmationHeading="Confirm removal" actionBtnName="Delete" actionBtnClassName="bg-red-500"/>
       <ButtonAdmin onClick={handleClick} className="bg-red-500">
         <FaTrashAlt className="text-white w-4 h-4" />
       </ButtonAdmin>
--- components/admin/general/ConfirmationDialog.jsx
+++ components/admin/general/ConfirmationDialog.jsx
@@ -7,11 +7,7 @@ import FocusLock from 'react-focus-lock';
 export default class ConfirmationDialog extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      show: false,
-    };
     this.Yoffset = 0
-    this.promiseInfo = {};
   }
   
 
@@ -21,35 +17,24 @@ export default class ConfirmationDialog extends React.Component {
     document.body.style.top = `-${this.Yoffset}px`
     document.body.style.left = '0px'
     document.body.style.right = '0px'
-    return new Promise((resolve, reject) => {
-      this.resolve = resolve;
-      this.reject = reject;
-      this.setState({
-        show: true,
-      });
-    });
+
   };
 
   hide = () => {
     document.body.setAttribute('style', '')
     window.scrollTo(0, this.Yoffset)
-    this.setState({
-      show: false,
-    });
   };
 
   render() {
-    const { show } = this.state;
     return (
       <>
-        {show && (
+        {this.props.show && (
           <Portal>
             <FocusLock>
             <div id="modalBackdrop" className="fixed top-0 left-0 right-0 bottom-0 backdrop-filter backdrop-blur-sm backdrop-brightness-75 z-50 flex items-center justify-center">
               <OutsideClickHandler
                 onOutsideClick={() => {
-                  this.hide();
-                  this.reject();
+                  this.props.onCancel();
                 }}
               >
                 <div className="bg-white shadow-lg border border-gray-100 p-4 flex flex-col gap-2 rounded-md max-w-lg">
@@ -58,8 +43,7 @@ export default class ConfirmationDialog extends React.Component {
                   <div className="flex gap-1 justify-center p-1">
                     <ButtonAdmin
                       onClick={() => {
-                        this.hide();
-                        this.reject();
+                        this.props.onCancel();
                       }}
                       className="bg-transparent text-gray-600"
                     >
@@ -68,8 +52,8 @@ export default class ConfirmationDialog extends React.Component {
                     <ButtonAdmin
                       className={this.props.actionBtnClassName}
                       onClick={() => {
-                        this.hide();
-                        this.resolve();
+                        this.props.onConfirm();
+                        
                       }}
                     >
                       {this.props.actionBtnName}
