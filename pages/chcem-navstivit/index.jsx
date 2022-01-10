515809c148b008e1a8f91220bde2a0873f7b0df1
Peter Smid <petosmid@gmail.com>
2022-01-10 16:52:06 +0100

    add featured image

--- pages/chcem-navstivit/index.jsx
+++ pages/chcem-navstivit/index.jsx
@@ -4,6 +4,8 @@ import { useFavoriteCamps } from '../../context/favoriteCampsContext';
 import Link from 'next/link';
 import { FaTrash } from 'react-icons/fa';
 import PopularDestinations from '../../components/base/Popular/PopularDestinations';
+import Image from 'next/image';
+import { STATIC_HOST } from '../../OPTIONS';
 
 function index() {
   const favorite = useFavoriteCamps();
@@ -30,19 +32,37 @@ function index() {
         Kempy ktoré plánujem navštíviť
       </h1>
       {favorite.camps.length === 0 && (
-        <div className='p-4 flex flex-col items-center gap-4'>
+        <div className="p-4 flex flex-col items-center gap-4">
           <p className="text-lg p-4 text-center text-pink-500">
             Zatiaľ nemáš pridané žiadne obľúbené kempy.
           </p>
-          <h2 className="text-2xl">Nájdi kemp v niektorých z obľúbených lokalít:</h2>
+          <h2 className="text-2xl">
+            Nájdi kemp v niektorých z obľúbených lokalít:
+          </h2>
           <PopularDestinations />
         </div>
       )}
       <ul className="p-2 divide-y divide-gray-300 max-w-3xl mx-auto">
         {favorite.camps.map(({ camp }) => {
+          const featuredImageSrc = camp.featured_image;
+
           return (
             <li key={camp._id} className={listItemStyle}>
-              <div className="flex">
+              <div className="flex gap-4">
+                <div className="col-span-1 relative">
+                  <div className="w-24 h-24">
+                    <Image
+                      src={
+                        featuredImageSrc
+                          ? `${STATIC_HOST}/${featuredImageSrc}`
+                          : '/camp_placeholder.png'
+                      }
+                      layout="fill"
+                      objectFit="cover"
+                    />
+                  </div>
+                </div>
+
                 <div className="flex flex-grow items-center">
                   <Link href={`kemp/${camp.slug}`}>
                     <a>
