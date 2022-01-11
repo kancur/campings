744d7512890e77ea7931bc0cf05df198db755264
Peter Smid <petosmid@gmail.com>
2022-01-11 12:59:47 +0100

    redesign

--- components/general/CampListing.jsx
+++ components/general/CampListing.jsx
@@ -13,36 +13,36 @@ export function CampListing({ camp, previewImage }) {
   return (
     <Link href={`/kemp/${camp.slug}`}>
       <a>
-      <div className="hover:shadow-lg hover:scale-101 duration-75 transition-all shadow-md border border-gray-100 p-3 bg-white grid gap-4 grid-cols-3 rounded-md max-w-screen-md flex-1">
-        <div className="col-span-1 relative">
-          <div className="w-48 h-48">
-            <Image
-              layout="fill"
-              objectFit="cover"
-              src={
-                previewImage
-                  ? previewImage
-                  : camp.featured_image
-                  ? featured
-                  : '/camp_placeholder.png'
-              }
-            />
+        <div className="overflow-hidden hover:shadow-lg hover:scale-101 duration-75 transition-all shadow-md bg-white grid grid-cols-3 rounded-lg max-w-screen-md flex-1">
+          <div className="col-span-1 relative">
+            <div className="w-48 h-48 rounded-md">
+              <Image
+                layout="fill"
+                objectFit="cover"
+                src={
+                  previewImage
+                    ? previewImage
+                    : camp.featured_image
+                    ? featured
+                    : '/camp_placeholder.png'
+                }
+              />
+            </div>
+          </div>
+          <div className="col-span-2 p-4">
+            <h2 className="text-2xl font-semibold">{camp?.name}</h2>
+            <div className="flex">
+              <p className="text-gray-400 text-s mb-2">
+                {parentVillage && `${parentVillage?.name}`}
+                {parentVillage?.parents?.county_name
+                  ? `, Okres ${parentVillage.parents.county_name}`
+                  : ''}
+              </p>
+            </div>
+            <p className="text-gray-500">{distance}</p>
+            <p className="">{camp?.shortDescription}</p>
           </div>
         </div>
-        <div className="col-span-2">
-          <h2 className="text-2xl font-semibold">{camp?.name}</h2>
-          <div className="flex">
-            <p className="text-gray-400 text-s mb-2">
-              {parentVillage && `${parentVillage?.name}`}
-              {parentVillage?.parents?.county_name
-                ? `, Okres ${parentVillage.parents.county_name}`
-                : ''}
-            </p>
-          </div>
-          <p className="text-gray-500">{distance}</p>
-          <p className="">{camp?.shortDescription}</p>
-        </div>
-      </div>
       </a>
     </Link>
   );
