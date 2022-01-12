6703ffc4c8aa09c7a6ed7d445baf9934d11db6f2
Peter Smid <petosmid@gmail.com>
2022-01-12 15:45:39 +0100

    add jsdoc comment

--- helpers/toSlug.js
+++ helpers/toSlug.js
@@ -1,13 +1,15 @@
+/**
+ * 
+ * @param {string} name string to convert to a slug
+ * @returns {string} Slug generated from input string
+ */
 export default function toSlug(name) {
   if (name) {
     const lowercased = name.toLowerCase();
     const trimmed = lowercased.trim();
-    const normalized = trimmed
-      .normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, '');
+    const normalized = trimmed.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
     const hyphenized = normalized.replace(/[^\w ]+/g, '').replace(/ +/g, '-');
     return hyphenized;
   }
   return '';
 }
-
