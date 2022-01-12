517de470437ff7bae62d3ff491e2b9d2aa002cf5
Peter Smid <petosmid@gmail.com>
2022-01-12 15:42:13 +0100

    refractor logic

--- middleware/authLoggedIn.jsx
+++ middleware/authLoggedIn.jsx
@@ -3,22 +3,22 @@ import { NextResponse } from 'next/server';
 // checking if user is logged in
 export async function authLoggedIn(req) {
   const cookies = req.cookies;
+
   if (!cookies.jwt) return NextResponse.redirect('/prihlasenie');
 
   try {
-    const response = await fetch(`${process.env.BACKEND_HOST}/api/auth/current-user`, {
-      headers: {
-        Cookie: `jwt=${cookies.jwt}`,
-      },
-    });
-
+    const response = await fetch(
+      `${process.env.BACKEND_HOST}/api/auth/current-user`,
+      {
+        headers: {
+          Cookie: `jwt=${cookies.jwt}`,
+        },
+      }
+    );
     const user = await response.json();
 
-    if (user) {
-      return NextResponse.next();
-    }
-
-    return NextResponse.redirect('/prihlasenie');
+    if (!user) return NextResponse.redirect('/prihlasenie');
+    return NextResponse.next();
 
   } catch (error) {
     return new Response(`Auth error: ${error.message}`, {
