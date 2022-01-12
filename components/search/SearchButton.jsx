30e5060d5be2eb6ca15d3b6a3c94b0bc646293ce
Peter Smid <petosmid@gmail.com>
2022-01-12 15:48:32 +0100

    lint

--- components/search/SearchButton.jsx
+++ components/search/SearchButton.jsx
@@ -1,8 +1,11 @@
-import { IoSearch } from "react-icons/io5";
+import { IoSearch } from 'react-icons/io5';
 
 export function SearchButton() {
   return (
-    <button type="submit" className="bg-amber-500 rounded-2xl p-3 text-gray-50 font-semibold flex items-center gap-1 focus:ring focus:ring-blue-400 focus:outline-none">
+    <button
+      type="submit"
+      className="bg-amber-500 rounded-2xl p-3 text-gray-50 font-semibold flex items-center gap-1 focus:ring focus:ring-blue-400 focus:outline-none"
+    >
       <IoSearch />
       Hľadať
     </button>
