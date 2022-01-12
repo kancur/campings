6cbed59350c58e82f37978a6c218e672e88de3fe
Peter Smid <petosmid@gmail.com>
2022-01-12 15:44:21 +0100

    lint

--- components/base/Popular/PopularCampings.jsx
+++ components/base/Popular/PopularCampings.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import SectionHeading from './SectionHeading'
+import React from 'react';
+import SectionHeading from './SectionHeading';
 
 function PopularCampings() {
   return (
     <div>
       <SectionHeading text="Obľúbené kempy" />
       <ul>
-        <li>
-          Camping
-        </li>
+        <li>Camping</li>
       </ul>
     </div>
-  )
+  );
 }
 
-export default PopularCampings
+export default PopularCampings;
