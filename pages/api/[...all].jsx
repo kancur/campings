4bf94d7eb81db250eb1ac5b9032f3968a009d9a0
Peter Smid <petosmid@gmail.com>
2022-01-12 15:58:24 +0100

    lint

--- pages/api/[...all].jsx
+++ pages/api/[...all].jsx
@@ -1,17 +1,21 @@
-import httpProxyMiddleware from "next-http-proxy-middleware"
+import httpProxyMiddleware from 'next-http-proxy-middleware';
 
-export default (req, res) => httpProxyMiddleware(req, res, {
-  // the /api/ url is automatically captured annd forwarded with the api call route
-  // no need to add /api to the url manually
-  target: (process.env.NODE_ENV === 'development') ? 'http://localhost:3000' : 'https://campings-express.herokuapp.com'
-  /* pathRewrite: [{
+export default (req, res) =>
+  httpProxyMiddleware(req, res, {
+    // the /api/ url is automatically captured and forwarded with the api call route
+    // no need to add /api to the url manually
+    target:
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:3000'
+        : 'https://campings-express.herokuapp.com',
+    /* pathRewrite: [{
     patternStr: '^/api/',
     replaceStr: '/'
   }] */
-});
+  });
 
 export const config = {
-    api: {
-        bodyParser: false
-    }
-}
+  api: {
+    bodyParser: false,
+  },
+};
