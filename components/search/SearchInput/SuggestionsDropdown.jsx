546800a9f153819ad98a0ecf36e06781e118a178
Peter Smid <petosmid@gmail.com>
2022-01-12 15:44:42 +0100

    lint

--- components/search/SearchInput/SuggestionsDropdown.jsx
+++ components/search/SearchInput/SuggestionsDropdown.jsx
@@ -14,11 +14,7 @@ import classNames from 'classnames';
 import OutsideClickHandler from 'react-outside-click-handler';
 
 const TentIcon = (props) => (
-  <svg
-    viewBox="0 0 455 455"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
+  <svg viewBox="0 0 455 455" xmlns="http://www.w3.org/2000/svg" {...props}>
     <path d="M430.473 415.471l-187.43-301.385 55.25-88.84-25.84-15.717-47.216 75.924L178.02 9.529l-25.84 15.716 55.25 88.841L20 415.471s-20 30 0 30h410.473c19.527 0 0-30 0-30zm-146.433 0L225.38 294.72l-58.906 120.751h-33.679s22.208-189.968 92.642-189.91c70.377.058 92.257 189.91 92.257 189.91z" />
   </svg>
 );
@@ -91,11 +87,9 @@ function getIcon(type) {
     case 'waterbody':
       return <FaWater className="suggestion-icon text-blue-500" />;
     case 'camp':
-      return <TentIcon className="suggestion-icon text-pink-500"/>;
+      return <TentIcon className="suggestion-icon text-pink-500" />;
     default:
       return <FaThumbtack className="suggestion-icon text-gray-500" />;
       break;
   }
 }
-
-
