335fade3ad740b54f91d63e50e5b30430cd26e52
Peter Smid <petosmid@gmail.com>
2022-01-12 15:48:43 +0100

    lint

--- components/locationPage/LocationHeading.jsx
+++ components/locationPage/LocationHeading.jsx
@@ -2,7 +2,9 @@ export default function LocationHeading({ pretitle, title, subtitle }) {
   return (
     <div className="text-center p-4 bg-gray-100 text-gray-500">
       {pretitle && <p className="text-lg">{pretitle}</p>}
-      <h1 className="text-4xl sm:text-5xl font-bold text-gray-700 p-2">{title}</h1>
+      <h1 className="text-4xl sm:text-5xl font-bold text-gray-700 p-2">
+        {title}
+      </h1>
       {subtitle && <p>{subtitle}</p>}
     </div>
   );
