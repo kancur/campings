e8ba2093514757cdb04a0182f845577840b886d0
Peter Smid <petosmid@gmail.com>
2022-01-12 15:49:28 +0100

    lint

--- components/general/FormWrapper.jsx
+++ components/general/FormWrapper.jsx
@@ -1,6 +1,6 @@
 import React, { Children } from 'react';
 
-function FormWrapper({title, children}) {
+function FormWrapper({ title, children }) {
   return (
     <div className="w-full h-full flex items-center justify-center bg-gradient-to-br from-violet-200 to-blue-300">
       <div className="p-6 max-w-md w-full shadow-md rounded-lg bg-gradient-to-br from-gray-100 to-gray-200 m-4">
