5c48ec2c3a493059adbba11f35a4cc662950358e
Peter Smid <petosmid@gmail.com>
2022-01-12 15:49:11 +0100

    lint and cleanup

--- components/general/LoadingSpinnerSVG.jsx
+++ components/general/LoadingSpinnerSVG.jsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import * as React from 'react';
 
 function LoadingSpinnerSVG(props) {
   return (
@@ -23,7 +23,7 @@ function LoadingSpinnerSVG(props) {
         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
       />
     </svg>
-  )
+  );
 }
 
-export default LoadingSpinnerSVG
+export default LoadingSpinnerSVG;
