fcfb5ef725c8942073be4de256b20a275c53c561
Peter Smid <petosmid@gmail.com>
2022-01-12 15:45:25 +0100

    refractor and lint

--- hooks/useDebouncedSearch.js
+++ hooks/useDebouncedSearch.js
@@ -1,31 +1,27 @@
-import AwesomeDebouncePromise from "awesome-debounce-promise";
-import { useState } from "react";
-import { useAsync } from "react-async-hook";
-import useConstant from "use-constant";
+import AwesomeDebouncePromise from 'awesome-debounce-promise';
+import { useState } from 'react';
+import { useAsync } from 'react-async-hook';
+import useConstant from 'use-constant';
+
+// debounce time in miliseconds
+const DEBOUNCE_TIME = 50;
 
 const useDebouncedSearch = (searchFunction) => {
   const [inputText, setInputText] = useState('');
 
   const debouncedSearchFunction = useConstant(() =>
-    AwesomeDebouncePromise(searchFunction, 50)
+    AwesomeDebouncePromise(searchFunction, DEBOUNCE_TIME)
   );
 
-  const searchResults = useAsync(
-    async () => {
-      if (inputText.length === 0) {
-        return [];
-      } else {
-        return debouncedSearchFunction(inputText);
-      }
-    },
-    [debouncedSearchFunction, inputText]
-  );
+  const searchResults = useAsync(async () => {
+    if (inputText.length === 0) {
+      return [];
+    } else {
+      return debouncedSearchFunction(inputText);
+    }
+  }, [debouncedSearchFunction, inputText]);
 
-  return [
-    inputText,
-    setInputText,
-    searchResults,
-  ];
+  return [inputText, setInputText, searchResults];
 };
 
-export default useDebouncedSearch
+export default useDebouncedSearch;
