5ad9bac366aa4a3e3e1301d6bae7ffb07c4e894f
Peter Smid <petosmid@gmail.com>
2022-01-14 19:57:46 +0100

    add interfaces

--- interfaces/baseInterfaces.ts
+++ interfaces/baseInterfaces.ts
@@ -10,14 +10,66 @@ export interface User {
   is_admin?: boolean;
 }
 
-export interface FavoriteCamp {
+export interface Camp {
   _id: string;
-  createdAt: string;
-  camp: {
-    _id: string;
-    name: string;
-    slug: string;
-    id: string;
+  name: string;
+  slug: string;
+  id?: string;
+}
+
+export interface CampListingInterface extends Camp {
+  featured_image?: string;
+  villages?: { parents?: { county_name: string | null }; name: string }[];
+  distance?: number;
+  shortDescription?: string;
+}
+
+export interface CampData extends Camp {
+  coords: Coords;
+  description?: string;
+  shortDescription?: string;
+  url?: string;
+  featuredImage?: string;
+  website?: string;
+}
+
+export interface Village {
+  distance: number;
+  lat: number;
+  lon: number;
+  name: string;
+  _id: string;
+  parents?: {
+    county_id?: number;
+    county_name?: string;
+    region_id?: number;
+    region_name?: string;
   };
 }
 
+export interface CampWithVillages extends Camp {
+  villages: Village[];
+}
+
+export interface FavoriteCamp {
+  _id: string;
+  createdAt: string;
+  camp: Camp;
+}
+
+export interface AuthContext {
+  user: User;
+  isLoading: boolean;
+  logout: () => void;
+}
+
+export type SearchTypes =
+  | 'village'
+  | 'mountain_range'
+  | 'basin'
+  | 'valley'
+  | 'flat'
+  | 'plain'
+  | 'plateau'
+  | 'waterbody'
+  | 'camp';
